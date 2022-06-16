81bd9117928ac3d7d61576102aa80214b223e227
franco esposito <faespo@live.com.ar>
2022-06-16 20:57:45 -0300

    ejercicios del modulo 3

---
+++ Ejercicio Sesion 3 - Ejercicio 1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Cliente cliente = new Cliente();
+
+Console.WriteLine(cliente);
+
+public struct Cliente {
+
+    public string? NombreCompleto { get; set; }
+    public double? Telefono { get; set; }
+    public string? Direccion { get; set; }
+    public string? Email { get; set; }
+    public bool? esNuevo { get; set; }
+    public Cliente() {
+
+        Console.WriteLine("Ingrese su Nombre Completo:");
+        NombreCompleto = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su Telefono:");
+        Telefono = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese su Direccion:");
+        Direccion = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su Email:");
+        Email = Console.ReadLine();
+
+        Console.WriteLine("es usted un cliente nuevo? s / n");
+        esNuevo = Console.ReadLine() == "s" ? true : false;
+    }
+    
+    public override string ToString() => $"(Nombre Completo: {NombreCompleto} \n Telefono: {Telefono} \n Direccion: {Direccion} \n Email: {Email}\n)";
+   
+}
