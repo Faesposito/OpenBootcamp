2022f24a49c4a72bf87661bb4c0277044dde4123
franco esposito <faespo@live.com.ar>
2022-06-17 12:22:16 -0300

    Ejercicio Sesion 5

---
+++ Ejercicio Sesion 5 - Ejercicio 1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int price = 100;
+Cliente cliente = new Cliente();
+
+if (cliente.HasCupon)
+{
+    Console.WriteLine($"Gracias a su Cupon el Precio a quedado en: {price - 20}");
+}
+else
+{
+    Console.WriteLine($"Gracias a su Cupon el Precio a quedado en: {price}");
+}
+public struct Cliente
+{
+
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public bool HasCupon { get; set; }
+    public Cliente()
+    {
+
+        Console.WriteLine("Ingrese su Nombre:");
+        Name = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su Email:");
+        Email = Console.ReadLine();
+
+        Console.WriteLine("Usted Posee un Cupon de Descuento? s / n");
+        HasCupon = Console.ReadLine() == "s" ? true : false;
+    }
+
+    public override string ToString() => $"(Nombre: {Name} \n Email: {Email}\n)";
+
+}
---
+++ Ejercicio Sesion 5 - Ejercicio 2/Program.cs
@@ -0,0 +1,28 @@
+﻿int number = 0;
+
+Console.WriteLine("Elija el Lenguage de Programacion:\n" +
+                  "1- C\n" +
+                  "2- C++\n" +
+                  "3- C#\n" +
+                  "4- Javascript\n");
+number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("El Lenguaje que usted eligio es C");
+        break;
+    case 2:
+        Console.WriteLine("El Lenguaje que usted eligio es C++");
+        break;
+    case 3:
+        Console.WriteLine("El Lenguaje que usted eligio es C#");
+        break;
+    case 4:
+        Console.WriteLine("El Lenguaje que usted eligio es Javascript");
+        break;
+
+    default:
+        Console.WriteLine("Usted No eligio ningun Lenguaje");
+        break;
+}
